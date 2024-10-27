3c8af2bb4c52d92980776b3e41eab43bccdb4e31
Jose Luis Ortiz <desarrollador@luisjdev.com>
2024-10-26 19:06:43 -0500

    Fixies and gtag

--- react-frontend/src/App.tsx
+++ react-frontend/src/App.tsx
@@ -4,18 +4,30 @@ import NavbarComponent from "./components/NavbarComponent"
 import OverviewComponent from "./components/OverviewComponent"
 import ProjectsBoxComponent from "./components/ProjectsBoxComponent"
 import SkillBoxComponent from "./components/SkillsBoxComponent"
+import TagManager from 'react-gtm-module'
+
+import { useEffect } from "react"
 
 function App() {
+  useEffect(() => {
+
+    const { VITE_ANALYTICS_TAG: gmtId } = import.meta.env
+
+    TagManager.initialize({
+      gtmId: gmtId
+    })
+  }, [])
+
   return (
     <main style={{
       marginBottom: '50px'
     }}>
       <NavbarComponent />
       <OverviewComponent />
-      <ExperienceBoxComponent/>
-      <SkillBoxComponent/>
-      <ProjectsBoxComponent/>
-      <ContactBoxComponent/>
+      <ExperienceBoxComponent />
+      <SkillBoxComponent />
+      <ProjectsBoxComponent />
+      <ContactBoxComponent />
     </main>
   )
 }
